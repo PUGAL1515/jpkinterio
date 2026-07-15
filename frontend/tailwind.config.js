68227f7c707d7d6feace7272616ca30bd6670d6b
smlnexgenllp <pugalendhi.smlnexgen@gmail.com>
2026-07-15 15:33:32 +0530

    changed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
